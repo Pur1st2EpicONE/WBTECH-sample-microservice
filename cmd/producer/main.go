3db11910e054a6a93d3a9315cc136aafa845ee19
Никита Четвёркин <ttkk2@mail.ru>
2025-08-12 23:17:15 +0200

    add producer

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,5 +1,82 @@
-package main
+package kafka
 
-func main() {
+import (
+	"fmt"
+	"strings"
+	"time"
 
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+)
+
+const (
+	flushTimeOutMs = 5000
+)
+
+type Producer struct {
+	producer *kafka.Producer
+}
+
+func NewProducer(clusterHosts []string) (*Producer, error) {
+	kafkaProducer, err := newKafkaProducer(clusterHosts)
+	if err != nil {
+		return nil, err
+	}
+	return &Producer{producer: kafkaProducer}, nil
+}
+
+func newKafkaProducer(clusterHosts []string) (*kafka.Producer, error) {
+	config := newConfig(clusterHosts)
+	producer, err := kafka.NewProducer(config)
+	if err != nil {
+		return nil, err
+	}
+	return producer, nil
+}
+
+func newConfig(clusterHosts []string) *kafka.ConfigMap {
+	return &kafka.ConfigMap{
+		"bootstrap.servers":  strings.Join(clusterHosts, ","),
+		"acks":               "all",
+		"retries":            3,
+		"linger.ms":          5,
+		"batch.size":         65536,
+		"compression.type":   "snappy",
+		"enable.idempotence": false,
+		"client.id":          "order-producer",
+	}
+}
+
+func (p *Producer) Produce(data string, topic string) error {
+	kafkaMessage, eventChan := newKafkaMessage(data, topic)
+	if err := p.producer.Produce(kafkaMessage, eventChan); err != nil {
+		return err
+	}
+	select {
+	case event := <-eventChan:
+		switch eventType := event.(type) {
+		case *kafka.Message:
+			if eventType.TopicPartition.Error != nil {
+				return eventType.TopicPartition.Error
+			}
+			return nil
+		default:
+			return fmt.Errorf("unknown event: %v", event)
+		}
+	case <-time.After(5 * time.Second):
+		return fmt.Errorf("event time out, make sure that Kafka is up and running")
+	}
+}
+
+func newKafkaMessage(data string, topic string) (*kafka.Message, chan kafka.Event) {
+	eventChan := make(chan kafka.Event)
+	return &kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Key:            nil,
+		Value:          []byte(data),
+	}, eventChan
+}
+
+func (p *Producer) Close() {
+	p.producer.Flush(flushTimeOutMs)
+	p.producer.Close()
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Pur1st2EpicONE/WBTECH-sample-microservice
+
+go 1.24.5
+
+require github.com/confluentinc/confluent-kafka-go/v2 v2.11.0
